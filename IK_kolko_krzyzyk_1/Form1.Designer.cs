5bff5f65fd6b0e6d9b7832483f434319288f6f68
Wojciech Szwarc <110970008+WojciechSzw@users.noreply.github.com>
2023-07-27 15:03:49 +0200

    Add files via upload

---
+++ IK_kolko_krzyzyk_1/Form1.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace IK_kolko_krzyzyk_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button11 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 25);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(66, 62);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(40, 40);
+            this.button2.TabIndex = 1;
+            this.button2.Text = " ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(112, 62);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(40, 40);
+            this.button3.TabIndex = 2;
+            this.button3.Text = " ";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.Location = new System.Drawing.Point(158, 62);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(40, 40);
+            this.button4.TabIndex = 3;
+            this.button4.Text = " ";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Enabled = false;
+            this.button5.Location = new System.Drawing.Point(66, 108);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(40, 40);
+            this.button5.TabIndex = 4;
+            this.button5.Text = " ";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Enabled = false;
+            this.button6.Location = new System.Drawing.Point(112, 108);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(40, 40);
+            this.button6.TabIndex = 5;
+            this.button6.Text = " ";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Enabled = false;
+            this.button7.Location = new System.Drawing.Point(158, 108);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(40, 40);
+            this.button7.TabIndex = 5;
+            this.button7.Text = " ";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Enabled = false;
+            this.button8.Location = new System.Drawing.Point(66, 154);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(40, 40);
+            this.button8.TabIndex = 6;
+            this.button8.Text = " ";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Enabled = false;
+            this.button9.Location = new System.Drawing.Point(112, 154);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(40, 40);
+            this.button9.TabIndex = 7;
+            this.button9.Text = " ";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Enabled = false;
+            this.button10.Location = new System.Drawing.Point(158, 154);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(40, 40);
+            this.button10.TabIndex = 8;
+            this.button10.Text = " ";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(90, 444);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(253, 22);
+            this.textBox1.TabIndex = 9;
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(66, 218);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(132, 23);
+            this.button11.TabIndex = 11;
+            this.button11.Text = "Nowa gra";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(211, 4);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 16);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Wynik:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(211, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 16);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "X: 0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(211, 36);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 16);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Y: 0";
+            // 
+            // label5
+            // 
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label5.AutoEllipsis = true;
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.White;
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.ForeColor = System.Drawing.Color.Crimson;
+            this.label5.Location = new System.Drawing.Point(66, 34);
+            this.label5.MinimumSize = new System.Drawing.Size(132, 20);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(132, 22);
+            this.label5.TabIndex = 15;
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label5.Visible = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(66, 416);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(290, 22);
+            this.textBox2.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(262, 253);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "X";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ IK_kolko_krzyzyk_1/Form1.cs
@@ -0,0 +1,294 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+
+
+
+
+namespace IK_kolko_krzyzyk_1
+{
+    public partial class Form1 : Form
+    {
+        string[] kik = { "", "", "", "", "", "", "", "", "", "0", "0", "0" };
+        string[] kikMemory = { "", "", "", "", "", "", "", "", "", "0", "0", "0" };
+        string turnMemory = "XO";
+        List<Button> buttons;
+
+        public Form1()
+        {
+            InitializeComponent();
+            serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(port_DataReceived_1);
+
+            buttons = new List<Button>()
+                {
+                    button2,button3,button4,button5,button6,button7,button8,button9,button10
+                };
+        }
+
+        private void port_DataReceived_1(object sender, SerialDataReceivedEventArgs eventArgs)
+        {
+
+            string InputData = serialPort1.ReadExisting();
+
+            kik = InputData.Split(',');
+
+
+            textBox1.BeginInvoke(new Action(() =>
+            {
+                textBox2.Clear();
+                textBox2.Text = InputData;
+
+                for (int i = 0; i < buttons.Count; i++)// if the kik is cler dont du tis
+                {
+                    buttons[i].Text = kik[i];
+
+                    if (buttons[i].Text != "")
+                    {
+                        buttons[i].Enabled = false;
+                    }
+                    else
+                    {
+                        buttons[i].Enabled = true;
+                    }
+                }
+
+                label3.Text = "X: " + kik[10];
+                label4.Text = "O: " + kik[11];
+
+                checkIfWin();
+
+                if (kik[9] == "0")
+                {
+                    label5.Visible = false;
+                }
+            }));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text == "Connect")
+            {
+                serialPort1.PortName = "COM1";
+                serialPort1.BaudRate = 600;
+                serialPort1.DataBits = Convert.ToInt16(5);
+                serialPort1.StopBits = (StopBits)Enum.Parse(typeof(StopBits), "One");
+                serialPort1.Handshake = (Handshake)Enum.Parse(typeof(Handshake), "None");
+                try
+                {
+                    serialPort1.Open();
+
+                    for (int i = 0; i < buttons.Count; i++)
+                    {
+                        buttons[i].Enabled = true;
+                    }
+                }
+                catch (UnauthorizedAccessException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+
+                button1.Text = "Connected";
+                button1.Enabled = false;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            send(0);
+            checkIfWin();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            send(1);
+            checkIfWin();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            send(2);
+            checkIfWin();
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            send(3);
+            checkIfWin();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            send(4);
+            checkIfWin();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            send(5);
+            checkIfWin();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            send(6);
+            checkIfWin();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            send(7);
+            checkIfWin();
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            send(8);
+            checkIfWin();
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            resetGame();
+            send(15);
+        }
+
+        private void send(int nrButtons)
+        {
+            
+
+            if (nrButtons == 15)//winO, tie or just unlocking buttons at the start info (the buttons r enabled for player X)
+            {
+                for (int i = 0; i < buttons.Count; i++)
+                {
+                    buttons[i].Enabled = true;
+                }
+            }
+            else if (nrButtons == 14)//winX info (the buttons r disabled for player X)
+            {
+                for (int i = 0; i < buttons.Count; i++)
+                {
+                    buttons[i].Enabled = false;
+                }
+            }
+            else if (nrButtons != 15)//usual info after move from player X
+            {
+                kik[nrButtons] = "X";
+                buttons[nrButtons].Text = "X";
+
+                for (int i = 0; i < buttons.Count; i++)
+                {
+                    buttons[i].Enabled = false;
+                }
+                kik[9] = "0";
+                label5.Visible = false;
+            }
+
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.Write(string.Join(",", kik));
+                textBox1.Clear();
+                textBox1.Text = string.Join(",", kik);
+            }
+        }
+
+        private void checkIfWin()
+        {
+            string player = "X";
+
+            if ((kik[0] == player && kik[1] == player && kik[2] == player)
+                || (kik[3] == player && kik[4] == player && kik[5] == player)
+                || (kik[6] == player && kik[7] == player && kik[8] == player)
+                || (kik[0] == player && kik[3] == player && kik[6] == player)
+                || (kik[1] == player && kik[4] == player && kik[7] == player)
+                || (kik[2] == player && kik[5] == player && kik[8] == player)
+                || (kik[0] == player && kik[4] == player && kik[8] == player)
+                || (kik[2] == player && kik[4] == player && kik[6] == player))
+            {
+                kik[9] = "1";
+                int x = int.Parse(kik[10]);
+                x++;
+                kik[10] = x.ToString();
+
+                label3.Text = "X: " + kik[10];
+                label4.Text = "O: " + kik[11];
+                label5.Text = "Win!";
+                label5.Visible = true;
+
+                resetTurn();
+                send(14);
+                return;
+            }
+
+            player = "O";
+
+            if ((kik[0] == player && kik[1] == player && kik[2] == player)
+            || (kik[3] == player && kik[4] == player && kik[5] == player)
+            || (kik[6] == player && kik[7] == player && kik[8] == player)
+            || (kik[0] == player && kik[3] == player && kik[6] == player)
+            || (kik[1] == player && kik[4] == player && kik[7] == player)
+            || (kik[2] == player && kik[5] == player && kik[8] == player)
+            || (kik[0] == player && kik[4] == player && kik[8] == player)
+            || (kik[2] == player && kik[4] == player && kik[6] == player))
+            {
+                kik[9] = "2";
+                int x = int.Parse(kik[11]);
+                x++;
+                kik[11] = x.ToString();
+
+                label3.Text = "X: " + kik[10];
+                label4.Text = "O: " + kik[11];
+                label5.Text = "Lose!";
+                label5.Visible = true;
+
+                resetTurn();
+                send(15);
+                return;
+            }
+            if (kik[0] != "" && kik[1] != "" && kik[2] != "" && kik[3] != "" && kik[4] != "" && kik[5] != "" && kik[6] != "" && kik[7] != "" && kik[8] != "")
+            {
+                kik[9] = "3";
+
+                label3.Text = "X: " + kik[10];
+                label4.Text = "O: " + kik[11];
+                label5.Text = "tie(fighter)";
+                label5.Visible = true;
+
+                resetTurn();
+                send(15);
+            }
+        }
+
+        private void resetTurn()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                kik[i] = "";
+                buttons[i].Text = "";
+            }
+        }
+
+        private void resetGame()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                kik[i] = "";
+                buttons[i].Text = kik[i];
+            }
+            kik[9] = "0";
+            kik[10] = "0";
+            kik[11] = "0";
+
+            label5.Visible = false;
+
+            label3.Text = "X: " + kik[10];
+            label4.Text = "O: " + kik[11];
+        }
+    }
+}
